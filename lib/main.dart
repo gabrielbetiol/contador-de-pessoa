5a202f887e9fe66d57fa7e1ab725f36313a961b9
gabrielbetiol <gabriel.betiol@gmail.com>
2020-04-14 19:26:42 -0300

    commit do projeto

---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = "Pode Entrar!";
+
+  void _changePeople(int number){
+    setState(() {
+      _people += number;
+
+      if(_people < 0){
+        _infoText = "Negativo :(";
+      } else if (_people > 10) {
+        _infoText = "Lotado!";
+      } else {
+        _infoText = "Pode Entrar!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "assets/images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.00,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: $_people",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text("+1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ),
+                FlatButton(
+                  child: Text("-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white)),
+                  onPressed: () {
+                    _changePeople(-1);
+                  },
+                ),
+              ],
+            ),
+            Text(
+              _infoText,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
